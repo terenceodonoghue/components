b7987a9f8325527de36dbc7ea379370debf152cc
Terence O'Donoghue <1697905+terenceodonoghue@users.noreply.github.com>
2022-07-16 17:19:53 +1000

    Create Slider React component

--- packages/react-components/src/velocity/Slider.tsx
+++ packages/react-components/src/velocity/Slider.tsx
@@ -5,22 +5,22 @@ import React, {
 } from 'react';
 import register from '@terenceodonoghue/web-components';
 
-const Switch: FunctionComponent<InputHTMLAttributes<HTMLInputElement>> = ({
+const Slider: FunctionComponent<InputHTMLAttributes<HTMLInputElement>> = ({
   className,
   style,
   ...props
 }) => {
   useEffect(() => {
     import('@terenceodonoghue/web-components/velocity').then(
-      ({ Switch: Component }) => register('wc-switch', Component),
+      ({ Slider: Component }) => register('wc-slider', Component),
     );
   }, []);
 
   return (
-    <wc-switch class={className} style={style}>
-      <input slot="input" type="checkbox" {...props} />
-    </wc-switch>
+    <wc-slider class={className} style={style}>
+      <input slot="input" type="range" {...props} />
+    </wc-slider>
   );
 };
 
-export default Switch;
+export default Slider;
